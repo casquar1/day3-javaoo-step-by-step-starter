d28cc717cfe229f0d8cd4b386106be8ecfb7fa57
Arianne <chiescake11@gmail.com>
2023-08-16 23:32:20 +0800

    feat: implement step 6

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,11 +1,14 @@
 package ooss;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Klass {
 
     private final int number;
     private Student leader;
+    private List<Person> persons = new ArrayList<>();
 
     public Klass(int number) {
         this.number = number;
@@ -36,11 +39,26 @@ public class Klass {
         if (student.isIn(this)) {
             setLeader(student);
             student.setLeader(true);
+            leaderMessage();
         }
         System.out.println("It is not one of us.");
     }
 
     public boolean isLeader(Student student) {
-        return leader.equals(student);
+        return leader != null && leader.equals(student);
+    }
+
+    public void attach(Person person) {
+        this.persons.add(person);
+    }
+
+    public void leaderMessage() {
+        for (Person person: persons) {
+            if (person instanceof Teacher) {
+                System.out.printf("I am %s, teacher of Class %d. I know %s become Leader.", person.getName(), number, leader.getName());
+            } else {
+                System.out.printf("I am %s, student of Class %d. I know %s become Leader.", person.getName(), number, leader.getName());
+            }
+        }
     }
 }
--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -13,6 +13,10 @@ public class Person {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public String introduce() {
         return String.format("My name is %s. I am %d years old.", name, age);
     }
